651f008caf06222b9e106c195a437f1c727ad5a4
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-26 21:39:32 -0800

    create better UI
    Co-authored-by: Logan Roth <diamondintheroth@gmail.com>
    Co-authored-by: Woo Jin Kim <kimwoojin211@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -39,16 +39,21 @@ $(document).ready(function() {
     const initialDeposit = parseInt($("#initialDeposit").val());
     let newAccount = new Account(name, initialDeposit);
     bank.addAccount(newAccount);
+    $("#output").hide();
+    $(".alert-success").show();
+    $(".alert-success").text(`Congrats! Your account has been created. Please remember your id = ${newAccount.id}. For now you have $ ${initialDeposit} available.`)
     $("#new-account")[0].reset();
   });
   $("#move-money").submit(function(event) {
     event.preventDefault();
+    $(".alert-success").hide();
     const id = parseInt($("#id").val());
     const deposit = parseInt($("#deposit").val());
     const withdraw = parseInt($("#withdraw").val());
     bank.accounts[id].deposit(deposit);
     bank.accounts[id].withdraw(withdraw);
     $("#move-money")[0].reset();
-    $("#output").text("$" + bank.accounts[id].amount);
+    $("#output").show();
+    $("#output").text("Your current ammount is $" + bank.accounts[id].amount);
   });
 });
